6804c0090e1fa1bd6b124e3ddf4072fd049205be
pranav1416 <pranav.borole@gmail.com>
2019-10-02 15:53:34 -0700

    Added Scoring to botvbot

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -2,6 +2,7 @@ from opgo import agent
 from opgo import goboard_slow
 from opgo import gotypes
 from opgo.utils import print_board, print_move 
+from opgo import scoring
 import time
 
 def main(): 
@@ -15,6 +16,7 @@ def main():
         bot_move = bots[game.next_player].select_move(game) 
         print_move(game.next_player, bot_move)
         game = game.apply_move(bot_move) 
-
+    game_result = scoring.compute_game_result(game)
+    print(game_result.winner,' wins!!\nMargin of win: ',game_result.winning_margin)
 if __name__ == '__main__':
         main()
