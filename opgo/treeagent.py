f825d04d77726cbba161ae10ff59f0637838e3fb
GaryKam <garyhkam@gmail.com>
2019-10-19 16:36:05 -0700

    mcts

--- opgo/treeagent.py
+++ opgo/treeagent.py
@@ -2,6 +2,8 @@ import math
 
 from opgo import agent
 from opgo.node import Node
+from opgo.gotypes import Player
+from opgo.agent.naivecfg import FastRandomBot
 
 
 def uct_score(parent_rollouts, child_rollouts, win_pct, temperature):
@@ -9,7 +11,23 @@ def uct_score(parent_rollouts, child_rollouts, win_pct, temperature):
     return win_pct + temperature * exploration
 
 
+def simulate_random_game(game):
+    bots = {
+        Player.black: FastRandomBot(),
+        Player.white: FastRandomBot(),
+    }
+    while not game.is_over():
+        bot_move = bots[game.next_player].select_move(game)
+        game = game.apply_move(bot_move)
+    return game.winner()
+
+
 class TreeAgent(agent.Agent):
+
+    def __init__(self, num_rounds, temperature):
+        agent.Agent.__init__(self)
+        self.num_rounds = num_rounds
+        self.temperature = temperature
 
     def select_move(self, game_state):
         root = Node(game_state)
@@ -22,7 +40,7 @@ class TreeAgent(agent.Agent):
                 if node.can_add_child():
                     node = node.add_random_child()
 
-                winner = self.simulate_random_game(node.game_state)
+                winner = simulate_random_game(node.game_state)
 
                 while node is not None:
                     node.record_win(winner)
